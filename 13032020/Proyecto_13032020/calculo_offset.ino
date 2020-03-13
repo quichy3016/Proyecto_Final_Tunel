362900ebea9120a6559b69105b8a0f19c7e9c9f2
quichy3016 <30843116+quichy3016@users.noreply.github.com>
2020-03-13 17:32:55 -0300

    actualice un poco

---
+++ 13032020/Proyecto_13032020/calculo_offset.ino
@@ -0,0 +1,18 @@
+void calculo_offset(){
+    if (contador==10){
+        offsett=t-tempref;
+        offsett1=t1-tempref;
+        offsett2=t2-tempref;
+        offseth=h-humref;
+        offseth1=h1-humref;
+        offseth2=h2-humref;
+        offsetp2=p2-presionref;
+        offsetdf=busvoltage;
+        offsetdf1=adc0;
+        cancelcontador=1;
+        contador=0;
+    }
+    if (cancelcontador==0){
+        contador=contador+1;
+    }
+}
