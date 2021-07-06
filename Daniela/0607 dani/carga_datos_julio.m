32c65414e1dac3bc125485d85f5b6fe68d934e4a
dcaamina <dcaamina@yahoo.com.ar>
2021-07-06 17:16:03 -0300

    matlab julio

--- Daniela/0607 dani/carga_datos_julio.m
+++ Daniela/0607 dani/carga_datos_julio.m
@@ -1,6 +1,6 @@
 
 %% CARGA DE DATOS
-[ALL]=csvread('Prueba_4.csv'); %%valor de muestras
+[ALL]=csvread('Prueba_6.csv'); %%valor de muestras
 [A]=ALL(:,2); %temp
 [B]=ALL(:,3); %hum1
 [C]=ALL(:,4); %hum
@@ -12,7 +12,8 @@
 [I]=ALL(:,10); %v s/filtro
 [J]=ALL(:,11); %v c/filtro
 [K]=ALL(:,12); %tiempo
-[L]=ALL(:,13);  %pwm             
+K=K/1000;
+[L]=ALL(:,13);  %pwm 
 [M]=ALL(:,14);  %control
 [T]=ALL(:,15); %v ref
 [O]=ALL(:,16); %error
@@ -23,3 +24,74 @@ plot(K,J,'k',K,T,'b');%v c/f. + v ref
 
 grid on
 grid minor
+
+
+%% DATOS DE LA PLANTA - Modificar
+%Datos que se deben modificar al estimar plantas.
+%Observar "ima1.png" e "ima2.png"
+Dy=0.813;%4.18; %
+Du=57;%300; % 
+tu=0.9;%1.64; %modificar STREJC
+ta=2;%1.42; %modificar STREJC
+TSS=1;
+
+%%%%%Parámetros de Strejc con retardo
+%%%%%(https://rua.ua.es/dspace/bitstream/10045/18965/1/Identificacion%20experimental%20de%20sistemas.pdf)
+%%%%%pag 9.
+tu1=tu-TSS;
+f=tu/ta;
+
+% tu1=tu-TSS;
+% f=tu1/ta;
+kS= Dy/Du;
+
+if (0<f)&&(f<=0.104)
+    n=2;
+    tS1=ta/2.7;
+    tS2=tu1/0.28;
+elseif (0.104<f)&&(f<=0.22)
+    n=3;
+    tS1=ta/3.7;
+    tS2=tu1/0.8;
+elseif (0.22<f)&&(f<=0.32)
+    n=4;    
+    tS1=ta/4.46;
+    tS2=tu1/1.42;
+elseif (0.32<f)&&(f<=0.46)
+    n=5;
+    tS1=ta/5.12;
+    tS2=tu1/2.1;
+else
+    print('error');
+end
+s=tf('s');
+S1=(kS/(tS1*s+1)^n); %prueba con un tao
+S2=(kS/(tS2*s+1)^n); %prueba con el otro tao
+S1_apro=pade(S1,1); %hace la funcion con nom/den polinomios
+S2_apro=pade(S2,1); 
+[nS1,dS1] = tfdata(S1_apro,'v');%num y den para utilizar en Simulink
+[nS2,dS2] = tfdata(S2_apro,'v');%num y den para utilizar en Simulink
+%%%%%%%%FIN ESTIMACIÓN DE PLANTA
+pause(1);
+
+%% SIMULINK Y MUESTRA DE GRAFICOS
+
+open_system('simu_julio');
+set_param(gcs,'SimulationMode','normal');
+set_param(gcs,'SimulationCommand','start');
+pause(2);
+
+
+for i=1:1:3082;
+    A11(i,1)=J(i,1); %v medida
+    T11(i,1)=K(i,1); %tiempo
+    O22(i,1)=L(i,1); %pwm
+end
+% 
+%esto es para tomar los valores del simulink!
+[A1]=ScopeData1.time;
+[B1]=ScopeData1.signals(:,1).values; %S1
+[D1]=ScopeData1.signals(:,3).values; %vref- set point
+figure(3)
+plot(A1,B1,'b',A1,D1,'g',T11,A11,'r')
+xlim([0 250]);
