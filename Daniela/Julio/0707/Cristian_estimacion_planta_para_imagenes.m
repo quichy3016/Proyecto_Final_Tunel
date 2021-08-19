d6241304b3ff638a40b9f0a0334a5df9e928598f
quichy3016 <30843116+quichy3016@users.noreply.github.com>
2021-08-19 16:04:42 -0300

    Cambios_en_uni

--- Daniela/Julio/0707/Cristian_estimacion_planta_para_imagenes.m
+++ Daniela/Julio/0707/Cristian_estimacion_planta_para_imagenes.m
@@ -1,5 +1,5 @@
 %% CARGA DE DATOS
-[ALL]=csvread('Prueba_6.csv'); %%valor de muestras
+[ALL]=csvread('Prueba_1.csv'); %%valor de muestras
 [A]=ALL(:,2); %temp
 [B]=ALL(:,3); %hum1
 [C]=ALL(:,4); %hum
@@ -19,8 +19,43 @@ K=K/1000;
 
 
 figure(1)
-plot(K,J,'k',K,T,'b');%v c/f. + v ref
+plot(K,J,'b',K,T,'k');%v c/f. + v ref
+xlim([110 135]);
+grid on
+grid minor
 
+figure(2)
+plot(K,J,'b',K,T+1.2,'k');%v c/f. + v ref
+x = [118.5 118.5];
+x1 = [120.1 120.1];
+x2 = [122.5 122.5];
+x3 = [125.6 125.6];
+x4 = [117 118.8];
+x5 = [117 122.3];
+y = [8.5 10];
+y1 = [9.7 9.7];
+y2 = [9.85 9.85];
+line(x,y,'Color','red','LineStyle','--')
+line(x1,y,'Color','red','LineStyle','--')
+line(x2,y,'Color','red','LineStyle','--')
+line(x3,y,'Color','red','LineStyle','--')
+line(x4,y1,'Color','red','LineStyle','--')
+line(x5,y2,'Color','red','LineStyle','--')
+xlim([110 135]);
+txt3 = ['T'];
+txt4 = ['$$t_p$$'];
+txt5 = ['$$T_{osc}/2$$'];
+txt6 = ['$$\Delta Y_{max} $$'];
+txt7 = ['$$\Delta Y(\infty) $$'];
+
+text(119.2,9.9,txt3,'Color','k','FontSize',15,'Interpreter','latex')
+text(121,9.9,txt4,'Color','k','FontSize',15,'Interpreter','latex')
+text(123.4,9.9,txt5,'Color','k','FontSize',15,'Interpreter','latex')
+text(115,9.85,txt6,'Color','k','FontSize',15,'Interpreter','latex')
+text(115,9.7,txt7,'Color','k','FontSize',15,'Interpreter','latex')
+xlabel('tiempo [s]','FontSize',14) 
+ylabel('velocidad [m/s]','FontSize',14) 
+legend({'Datos Experimentales','Escalon'},'Location','southeast','FontSize',14)
 grid on
 grid minor
 
@@ -66,14 +101,14 @@ Q_apro=pade(Q,1); %muestra la tf en forma de polinomio
 [D1]=ScopeData.signals(:,3).values; %vref- set point
 figure(3)
 subplot(2,1,1)
-plot(A1,B1,'b',A1,D1,'k',K,J,'r')
-xlim([50 255]);
+plot(A1,D1,'k',K,J,'r')
+xlim([70 250]);
 xlabel('tiempo [s]') 
 ylabel('velocidad [m/s]') 
-legend({'Planta estimada','Escalon', 'v medida'},'Location','northwest')
+legend({'Escalon','v medida' },'Location','northwest')
 subplot(2,1,2)
-plot(A1,B1,'b',A1,D1,'k',K,J,'r')
-xlim([50 255]);
+plot(A1,D1,'k',A1,B1,'b')
+xlim([70 250]);
 xlabel('tiempo [s]') 
 ylabel('velocidad [m/s]') 
-legend({'Planta estimada','Escalon', 'v medida'},'Location','northwest')
+legend({'Escalon','Planta estimada'},'Location','northwest')
